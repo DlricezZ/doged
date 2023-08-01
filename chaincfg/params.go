f175d0bb6290b888be82c15fa49681c3e6aa72e6
liucan <liucan@hashkey.com>
2023-08-01 16:17:04 +0800

    update chaincfg params

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -403,24 +403,37 @@ var MainNetParams = Params{
 	// Mempool parameters
 	RelayNonStdTxs: false,
 
-	// Human-readable part for Bech32 encoded segwit addresses, as defined in
-	// BIP 173.
-	Bech32HRPSegwit: "bc", // always bc for main net
+	//// Human-readable part for Bech32 encoded segwit addresses, as defined in
+	//// BIP 173.
+	//Bech32HRPSegwit: "bc", // always bc for main net
+	//
+	//// Address encoding magics
+	//PubKeyHashAddrID:        0x00, // starts with 1
+	//ScriptHashAddrID:        0x05, // starts with 3
+	//PrivateKeyID:            0x80, // starts with 5 (uncompressed) or K (compressed)
+	//WitnessPubKeyHashAddrID: 0x06, // starts with p2
+	//WitnessScriptHashAddrID: 0x0A, // starts with 7Xh
+	//
+	//// BIP32 hierarchical deterministic extended key magics
+	//HDPrivateKeyID: [4]byte{0x04, 0x88, 0xad, 0xe4}, // starts with xprv
+	//HDPublicKeyID:  [4]byte{0x04, 0x88, 0xb2, 0x1e}, // starts with xpub
+	//
+	//// BIP44 coin type used in the hierarchical deterministic path for
+	//// address generation.
+	//HDCoinType: 0,
 
-	// Address encoding magics
-	PubKeyHashAddrID:        0x00, // starts with 1
-	ScriptHashAddrID:        0x05, // starts with 3
-	PrivateKeyID:            0x80, // starts with 5 (uncompressed) or K (compressed)
-	WitnessPubKeyHashAddrID: 0x06, // starts with p2
-	WitnessScriptHashAddrID: 0x0A, // starts with 7Xh
+	Bech32HRPSegwit: "doge",
 
-	// BIP32 hierarchical deterministic extended key magics
-	HDPrivateKeyID: [4]byte{0x04, 0x88, 0xad, 0xe4}, // starts with xprv
-	HDPublicKeyID:  [4]byte{0x04, 0x88, 0xb2, 0x1e}, // starts with xpub
+	PubKeyHashAddrID:        0x1e,
+	ScriptHashAddrID:        0x16,
+	PrivateKeyID:            0x9e,
+	WitnessPubKeyHashAddrID: 0x00,
+	WitnessScriptHashAddrID: 0x00,
 
-	// BIP44 coin type used in the hierarchical deterministic path for
-	// address generation.
-	HDCoinType: 0,
+	HDPublicKeyID:  [4]byte{0x02, 0xfa, 0xca, 0xfd},
+	HDPrivateKeyID: [4]byte{0x02, 0xfa, 0xc3, 0x98},
+
+	HDCoinType: 3,
 }
 
 // RegressionNetParams defines the network parameters for the regression test
@@ -437,7 +450,6 @@ var RegressionNetParams = Params{
 	GenesisHash:              &regTestGenesisHash,
 	PowLimit:                 regressionPowLimit,
 	PowLimitBits:             0x207fffff,
-	PoWNoRetargeting:         true,
 	CoinbaseMaturity:         100,
 	BIP0034Height:            100000000, // Not active - Permit ver 1 blocks
 	BIP0065Height:            1351,      // Used by regression tests
@@ -649,14 +661,30 @@ var TestNet3Params = Params{
 
 	// Human-readable part for Bech32 encoded segwit addresses, as defined in
 	// BIP 173.
-	Bech32HRPSegwit: "tb", // always tb for test net
+	// Bech32HRPSegwit: "tb", // always tb for test net
+
+	// // Address encoding magics
+	// PubKeyHashAddrID:        0x6f, // starts with m or n
+	// ScriptHashAddrID:        0xc4, // starts with 2
+	// WitnessPubKeyHashAddrID: 0x03, // starts with QW
+	// WitnessScriptHashAddrID: 0x28, // starts with T7n
+	// PrivateKeyID:            0xef, // starts with 9 (uncompressed) or c (compressed)
+
+	// // BIP32 hierarchical deterministic extended key magics
+	// HDPrivateKeyID: [4]byte{0x04, 0x35, 0x83, 0x94}, // starts with tprv
+	// HDPublicKeyID:  [4]byte{0x04, 0x35, 0x87, 0xcf}, // starts with tpub
+
+	// // BIP44 coin type used in the hierarchical deterministic path for
+	// // address generation.
+	// HDCoinType: 1,
+	Bech32HRPSegwit: "dog", // always tb for test net
 
 	// Address encoding magics
-	PubKeyHashAddrID:        0x6f, // starts with m or n
+	PubKeyHashAddrID:        0x71, // starts with m or n
 	ScriptHashAddrID:        0xc4, // starts with 2
 	WitnessPubKeyHashAddrID: 0x03, // starts with QW
 	WitnessScriptHashAddrID: 0x28, // starts with T7n
-	PrivateKeyID:            0xef, // starts with 9 (uncompressed) or c (compressed)
+	PrivateKeyID:            0xf1, // starts with 9 (uncompressed) or c (compressed)
 
 	// BIP32 hierarchical deterministic extended key magics
 	HDPrivateKeyID: [4]byte{0x04, 0x35, 0x83, 0x94}, // starts with tprv
